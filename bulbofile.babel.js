16b980d715ba8a8abc76591c8c7185931426287e
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-06 00:39:59 +0900

    Implement frontend

--- bulbofile.babel.js
+++ bulbofile.babel.js
@@ -2,11 +2,13 @@ import {asset, dest} from 'bulbo'
 import through from 'through'
 import browserify from 'browserify'
 
-asset('src/site/index.html')(src => src)
+asset('src/site/index.html')
 
 asset('src/site/js/index.js', {base: 'src/site'})(src => src.pipe(through(function (file) {
   file.contents = browserify(file.path).transform('babelify').bundle()
   this.queue(file)
 })))
+
+asset('src/site/css/**/*', {base: 'src/site'})
 
 dest('site')
--- src/site/js/index.js
+++ src/site/js/index.js
@@ -1,2 +1,43 @@
-import $ from 'jquery'
+import './globals'
 import 'bootstrap'
+import {Promise} from 'es6-promise'
+
+const PROJECT_API = '/api/projects'
+
+function main() {
+
+  Promise.resolve($.get(PROJECT_API)).then(projects => {
+
+    projects.forEach(project => renderProject(project))
+
+  })
+
+}
+
+function renderProject(project) {
+  const main = '.main'
+
+  $('<h2/>').text(project.title).appendTo(main)
+
+  const container = $('<div class="container"/>').appendTo(main)
+
+  $('<h3/>').text('TODO').appendTo(container)
+
+  const todoList = $('<ul/>').appendTo(container)
+
+  project.todos.forEach(todo => {
+    $('<li/>').text(todo.title).appendTo(todoList)
+  })
+
+  $('<h3/>').text('DONE').appendTo(container)
+
+  const doneList = $('<ul/>').appendTo(container)
+
+  project.dones.forEach(done => {
+    $('<li/>').text(done.title).appendTo(doneList)
+  })
+
+  $('<hr />').appendTo('.main')
+}
+
+main()
