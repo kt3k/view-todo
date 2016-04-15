db18b2b91bdaf34273611d3f056f923b0b213146
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-15 23:01:33 +0900

    Update bulbo

--- bulbofile.babel.js
+++ bulbofile.babel.js
@@ -4,11 +4,14 @@ import browserify from 'browserify'
 
 asset('src/site/index.html')
 
-asset('src/site/js/index.js', {base: 'src/site'})(src => src.pipe(through(function (file) {
+asset('src/site/js/index.js')
+.base('src/site')
+.pipe(through(function (file) {
   file.contents = browserify(file.path).transform('babelify').bundle()
   this.queue(file)
-})))
+}))
 
-asset('src/site/css/**/*', {base: 'src/site'})
+asset('src/site/css/**/*')
+.base('src/site')
 
 dest('site')
