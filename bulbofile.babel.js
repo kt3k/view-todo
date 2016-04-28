35a48431fca39072cc8c1246a6d0094673375ae4
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-28 23:34:52 +0900

    Update bulbofile

--- bulbofile.babel.js
+++ bulbofile.babel.js
@@ -6,7 +6,7 @@ asset('src/site/index.html')
 
 asset('src/site/js/index.js')
 .base('src/site')
-.pipe(through2(function (file, enc, callback) {
+.pipe(through2.obj(function (file, enc, callback) {
   file.contents = browserify(file.path).transform('babelify').bundle()
   this.push(file)
   callback()
